c085dd8dfd51cee6364abc3ded67856ccc5e932e
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-20 20:49:39 +0600

    Update 2. Primality Test.cpp

--- 2. Primality Test.cpp
+++ 2. Primality Test.cpp
@@ -1,5 +1,38 @@
 // url = https://www.codechef.com/problems/PRB01
 
+// solution using sieve algorithm
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int x[100002];
+int len = sizeof(x)/4;
+int main()
+{
+    for(int i=0; i<len; x[i++]=1);
+    x[0] = 0;
+    x[1] = 0;
+    for(int i=2; i<=sqrt(len); i++){
+        if(x[i]){
+            for(int j=i+i; j<len; j+=i){
+                x[j] = 0;
+            }
+        }
+    }
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(x[n]) cout<<"yes"<<endl;
+        else cout<<"no"<<endl;
+    }
+    return 0;
+}
+
+
+// solution using normal way
+
 #include <bits/stdc++.h>
 using namespace std;
 
