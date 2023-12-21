24df228a74662f4ab79c4e26d96983c3772d9eaf
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-21 08:14:16 +0600

    Create 4. Thala For A Reason.cpp

---
+++ 4. Thala For A Reason.cpp
@@ -0,0 +1,12 @@
+// url = https://www.codechef.com/problems/THALA7
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	if(n==7) cout<<"THALA"<<endl;
+	else cout<<"SADGE"<<endl;
+	return 0;
+}
