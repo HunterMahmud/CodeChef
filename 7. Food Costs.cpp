151352da5eac3e4b9d33684992de9b9c60af51a7
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-24 07:47:21 +0600

    Create 7. Food Costs.cpp

---
+++ 7. Food Costs.cpp
@@ -0,0 +1,12 @@
+// url = https://www.codechef.com/problems/FOODCOST
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	cin>>x>>y;
+	cout<<(x*6 + y)<<endl;
+    return 0;
+}
