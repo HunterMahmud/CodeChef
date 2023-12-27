36166c3fe7ec61dc96fea5ebf40bfc8e824ddc28
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 15:49:33 +0600

    Create 37. Parity.cpp

---
+++ 37. Parity.cpp
@@ -0,0 +1,16 @@
+// url = https://www.codechef.com/problems/PAR2
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n%2==0) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
