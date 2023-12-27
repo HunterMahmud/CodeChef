90a57f2cf98a05f39f7a1bc30721f0b59372e1cd
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 12:56:34 +0600

    Create 28 .Bone Appetit.cpp

---
+++ 28 .Bone Appetit.cpp
@@ -0,0 +1,12 @@
+// url = https://www.codechef.com/problems/BNE_APT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int a,b,c,d;
+    cin>>a>>b;
+    cin>>c>>d;
+    cout<<(a*c)+(d*b)<<endl;
+}
