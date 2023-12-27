fdd21434c1202518dcd19954fac9a8b824252119
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 15:44:36 +0600

    Create 35. Candy Division.cpp

---
+++ 35. Candy Division.cpp
@@ -0,0 +1,16 @@
+// url = https://www.codechef.com/problems/CANDIVIDE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n%3==0) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
