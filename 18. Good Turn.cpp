e05af38a17de321aab0e7802d3bc868d0d41b368
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 12:06:09 +0600

    Create 18. Good Turn.cpp

---
+++ 18. Good Turn.cpp
@@ -0,0 +1,17 @@
+// url = https://www.codechef.com/problems/GDTURN
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    if(x+y>6) cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+	return 0;
+}
