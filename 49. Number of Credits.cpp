421417ce1df8c6ab7ee2fc61336f5773f476f9a1
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 17:12:04 +0600

    Create 49. Number of Credits.cpp

---
+++ 49. Number of Credits.cpp
@@ -0,0 +1,15 @@
+// url = https://www.codechef.com/problems/CREDS
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        cout<<a*4+b*2<<endl;
+    }
+}
