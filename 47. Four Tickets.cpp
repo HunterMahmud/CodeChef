fb0140f5281b594ebcc77d924fa6169a78c83134
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 17:07:08 +0600

    Create 47. Four Tickets.cpp

---
+++ 47. Four Tickets.cpp
@@ -0,0 +1,17 @@
+// url = https://www.codechef.com/problems/FOURTICKETS
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(4*n>1000) cout<<"NO"<<endl;
+	    else cout<<"YES"<<endl;
+	}
+
+}
