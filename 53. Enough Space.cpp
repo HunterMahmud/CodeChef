d954e0646ef7cc4bc0271544ab6cb856af0e32b8
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 17:22:28 +0600

    Create 53. Enough Space.cpp

---
+++ 53. Enough Space.cpp
@@ -0,0 +1,17 @@
+// url = https://www.codechef.com/problems/ENSPACE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x,y;
+        cin>>n>>x>>y;
+        if(x+2*y<=n) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+        
+    }
+}
