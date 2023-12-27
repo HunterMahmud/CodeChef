a633cfa24810c619e0dd1e1b74b415524df93bc8
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 12:09:01 +0600

    Create 19. Masterchef finals.cpp

---
+++ 19. Masterchef finals.cpp
@@ -0,0 +1,17 @@
+// url = https://www.codechef.com/problems/TOP10
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n<=10) cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+
+}
