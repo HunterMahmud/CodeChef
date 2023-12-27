8e8c8847d8575f3c8eff0392cdefe17c6c8eb24a
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 12:17:12 +0600

    Create 21. Kitchen Timings.cpp

---
+++ 21. Kitchen Timings.cpp
@@ -0,0 +1,16 @@
+// url = https://www.codechef.com/problems/KITCHENTIME
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    cout<<(y-x)<<endl;
+	}
+
+}
