d35ee5da738d55a42bf1d13cb276cb6abec2d131
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 12:29:48 +0600

    Create 23. Fan Poll.cpp

---
+++ 23. Fan Poll.cpp
@@ -0,0 +1,13 @@
+// url = https://www.codechef.com/problems/FIZZBUZZ2301
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int d,r,k;
+	cin>>d>>r>>k;
+	if(d>r && d>k) cout<<"YES"<<endl;
+	else cout<<"NO"<<endl;
+
+}
