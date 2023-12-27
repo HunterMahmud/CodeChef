0d28cbbde8557bccd36ef217a242144ed5260b3c
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 16:32:04 +0600

    Create 42. Ageing.cpp

---
+++ 42. Ageing.cpp
@@ -0,0 +1,14 @@
+// url = https://www.codechef.com/problems/AGEING
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<n-10<<endl;
+    }
+}
