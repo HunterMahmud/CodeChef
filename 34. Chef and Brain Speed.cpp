89e6e1bca798918e275aa6dc8b1e3a0f47680ad7
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 15:42:17 +0600

    Create 34. Chef and Brain Speed.cpp

---
+++ 34. Chef and Brain Speed.cpp
@@ -0,0 +1,13 @@
+// url = https://www.codechef.com/problems/CBSPEED
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b;
+	cin>>a>>b;
+	if(a<b) cout<<"YES"<<endl;
+	else cout<<"NO"<<endl;
+	return 0;
+}
