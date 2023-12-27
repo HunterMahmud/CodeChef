b0e13a1d57df860bd591e09299a32909c99b2896
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 15:37:57 +0600

    Create 33. Roller Coaster.cpp

---
+++ 33. Roller Coaster.cpp
@@ -0,0 +1,16 @@
+// url = https://www.codechef.com/problems/MINHEIGHT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        if(x>=y) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
