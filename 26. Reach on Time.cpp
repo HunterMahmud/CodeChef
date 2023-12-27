9b8c0d270c3ff220cf40c263b1847951e88049ec
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 12:46:32 +0600

    Create 26. Reach on Time.cpp

---
+++ 26. Reach on Time.cpp
@@ -0,0 +1,16 @@
+// url = https://www.codechef.com/problems/TIMELY
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n>29) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
