67cf333995572e1ccf7df4c736b62727bb496e82
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 15:46:49 +0600

    Create 36. Chef On Date.cpp

---
+++ 36. Chef On Date.cpp
@@ -0,0 +1,17 @@
+// url = https://www.codechef.com/problems/CHEFONDATE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    if(x>=y) cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+
+}
