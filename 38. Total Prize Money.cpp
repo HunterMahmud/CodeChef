18baddafb00f463a4964d46106ebea304c475251
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 15:52:24 +0600

    Create 38. Total Prize Money.cpp

---
+++ 38. Total Prize Money.cpp
@@ -0,0 +1,16 @@
+// url = https://www.codechef.com/problems/PRIZEPOOL
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    cout<<x*10+y*90<<endl;
+	}
+
+}
