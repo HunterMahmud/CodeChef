99ba22c66ad2c1531a5c9abccd49cca06ddc550a
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 12:43:06 +0600

    Create 25. Audible Range.cpp

---
+++ 25. Audible Range.cpp
@@ -0,0 +1,16 @@
+// url = https://www.codechef.com/problems/AUDIBLE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int b;
+        cin>>b;
+        if(b>66 && b<45001) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
