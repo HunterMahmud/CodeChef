453f4452cb56b2de7e9b6f7d2e1e26466188805a
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 17:16:49 +0600

    Create 51. Get Subscription.cpp

---
+++ 51. Get Subscription.cpp
@@ -0,0 +1,16 @@
+// url = https://www.codechef.com/problems/SUBSCRIBE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n<=30) cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
+    }
+}
