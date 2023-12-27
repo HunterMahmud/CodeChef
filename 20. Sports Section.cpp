ade6a654835da97be864b2e44a76ae5499f381b2
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 12:12:15 +0600

    Create 20. Sports Section.cpp

---
+++ 20. Sports Section.cpp
@@ -0,0 +1,13 @@
+// url = https://www.codechef.com/problems/NEWSPAPER
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	if(n>7) cout<<"YES"<<endl;
+	else cout<<"NO"<<endl;
+
+}
