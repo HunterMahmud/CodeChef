98933d35ca0da400d7e4d272b86e2a6701d5df51
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 13:06:57 +0600

    Create 31. Tour of King.cpp

---
+++ 31. Tour of King.cpp
@@ -0,0 +1,15 @@
+// url = https://www.codechef.com/problems/KNGTOR
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m;
+        cin>>n>>m;
+        cout<<n*5+m*7<<endl;
+    }
+}
