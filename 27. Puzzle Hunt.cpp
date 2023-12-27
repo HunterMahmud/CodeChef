8db5e4490f9b4b8cdc63e34319ec277b243dd37d
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 12:51:21 +0600

    Create 27. Puzzle Hunt.cpp

---
+++ 27. Puzzle Hunt.cpp
@@ -0,0 +1,13 @@
+// url = https://www.codechef.com/problems/PUZHUNT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	if(n>5 && n<9) cout<<"YES"<<endl;
+	else cout<<"NO"<<endl;
+
+}
