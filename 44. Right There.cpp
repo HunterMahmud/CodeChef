1b532798df584266a085853fc896f6984e4907d8
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 16:38:41 +0600

    Create 44. Right There.cpp

---
+++ 44. Right There.cpp
@@ -0,0 +1,16 @@
+// url = https://www.codechef.com/problems/RIGHTTHERE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+        if(n<=x) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
