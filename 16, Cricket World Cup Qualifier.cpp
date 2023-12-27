e2d4f490bffc1951d13833e36598c29fe9bb0197
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 09:10:34 +0600

    Create 16, Cricket World Cup Qualifier.cpp

---
+++ 16, Cricket World Cup Qualifier.cpp
@@ -0,0 +1,13 @@
+// url = https://www.codechef.com/problems/CWC23QUALIF
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int p;
+    cin>>p;
+    if(p>=12) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+    return 0;
+}
