26b4afeb8aa7faaf6b3a300b4a66f21b11a193ff
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 12:38:37 +0600

    Create 24. Tax in Chefland.cpp

---
+++ 24. Tax in Chefland.cpp
@@ -0,0 +1,17 @@
+// url = https://www.codechef.com/problems/TAXES
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n>100) cout<<n-10<<endl;
+	    else cout<<n<<endl;
+	}
+
+}
