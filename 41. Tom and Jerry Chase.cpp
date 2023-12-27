f294d2bfc9db48a947ffcc9454697dc4fa3f219e
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-27 16:06:09 +0600

    Create 41. Tom and Jerry Chase.cpp

---
+++ 41. Tom and Jerry Chase.cpp
@@ -0,0 +1,17 @@
+// url = https://www.codechef.com/problems/JERRYCHASE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int j,tm;
+        cin>>j>>tm;
+        if(j<tm) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+        
+    }
+}
