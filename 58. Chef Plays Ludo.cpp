676157bbfd81da4be135f2da542846adc0a1511f
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-28 10:30:19 +0600

    Create 58. Chef Plays Ludo.cpp

---
+++ 58. Chef Plays Ludo.cpp
@@ -0,0 +1,16 @@
+// url = https://www.codechef.com/problems/LUDO
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n==6) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
