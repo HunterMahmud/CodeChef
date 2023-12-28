ef0dc707cd4a4e515c292e6bcabd9a5e8dee997c
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-28 11:21:34 +0600

    Create 73. Cars and Bikes.cpp

---
+++ 73. Cars and Bikes.cpp
@@ -0,0 +1,16 @@
+// url = https://www.codechef.com/problems/TYRES
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n%4==0) cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
+    }
+}
