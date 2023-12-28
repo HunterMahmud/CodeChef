0d0970554e2cdfd1da1ac5f46e492c9e78069ea2
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-28 11:12:24 +0600

    Create 71. Insurance.cpp

---
+++ 71. Insurance.cpp
@@ -0,0 +1,16 @@
+// url = https://www.codechef.com/problems/INSURANCE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        if(x>=y) cout<<y<<endl;
+        else cout<<x<<endl;
+    }
+}
