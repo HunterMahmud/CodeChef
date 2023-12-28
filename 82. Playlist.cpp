f57898e4f68d65ac56b7af19f8c5ea38fbdf1e9d
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-28 21:30:16 +0600

    Create 82. Playlist.cpp

---
+++ 82. Playlist.cpp
@@ -0,0 +1,15 @@
+// url = https://www.codechef.com/problems/SONGS
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+        cout<<(n/(x*3))<<endl;
+    }
+}
