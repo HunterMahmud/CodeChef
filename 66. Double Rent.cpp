24aaeadc3217ec7675601bc3f526beb2f0d84f30
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-28 10:53:42 +0600

    Create 66. Double Rent.cpp

---
+++ 66. Double Rent.cpp
@@ -0,0 +1,11 @@
+// url = https://www.codechef.com/problems/DOUBLERENT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n;
+    cin>>n;
+    cout<<2*n<<endl;
+}
