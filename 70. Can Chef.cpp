d3a5b4c4998151e4c9faa3c64ec1b5885a660420
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-28 11:08:13 +0600

    Create 70. Can Chef.cpp

---
+++ 70. Can Chef.cpp
@@ -0,0 +1,16 @@
+// url = https://codechef.com/problems/CANCHEF
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        if(x*15>=y*2) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
