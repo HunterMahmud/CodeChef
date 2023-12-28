dc6d3614dfa287d8511e5020f85a30e4df5e6ecf
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-28 10:51:15 +0600

    Create 64. Lucky Seven.cpp

---
+++ 64. Lucky Seven.cpp
@@ -0,0 +1,11 @@
+// url = https://www.codechef.com/problems/LUCKYSEVEN
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    string s;
+    cin>>s;
+    cout<<s[6]<<endl;
+}
