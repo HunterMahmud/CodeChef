0c19282fbca8e6bc9772e3a8e6b212c2878d06fc
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-28 11:27:28 +0600

    Create 75. Guess the bottom face.cpp

---
+++ 75. Guess the bottom face.cpp
@@ -0,0 +1,14 @@
+// url = https://www.codechef.com/problems/BOTTOM
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<7-n<<endl;
+    }
+}
