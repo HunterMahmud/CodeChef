2ed7e08a89de520d7c1b6a01d3492f9b072f8dd7
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-28 21:11:34 +0600

    Create 79. Water Park.cpp

---
+++ 79. Water Park.cpp
@@ -0,0 +1,12 @@
+// url = https://www.codechef.com/problems/SPCP1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int w,h;
+    cin>>w>>h;
+    if(w>=60 && h<=130) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
