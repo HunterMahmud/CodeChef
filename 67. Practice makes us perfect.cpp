4c8143ad155d2f3ea9e2695d0785d7e32cf15d2f
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-28 10:59:01 +0600

    Create 67. Practice makes us perfect.cpp

---
+++ 67. Practice makes us perfect.cpp
@@ -0,0 +1,16 @@
+// url = https://www.codechef.com/problems/PRACTICEPERF
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int cnt = 0;
+    int p;
+    int t=4;
+    while(t--){
+        cin>>p
+        if(p>9) cnt++;
+    }
+    cout<<cnt<<endl;
+}
