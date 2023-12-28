8a6fd1de3eb11d491fcfad5b018aa3fb6f971ebf
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-28 10:42:23 +0600

    Create 61. Determine the Score.cpp

---
+++ 61. Determine the Score.cpp
@@ -0,0 +1,15 @@
+// url = https://www.codechef.com/problems/DETSCORE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int x,n;
+        cin>>x>>n;
+        cout<<(x/10)*n<<endl;
+    }
+}
