91ddd6d14ea967849203d7567815a470aec3f172
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-28 10:47:04 +0600

    Create 62. Off By One.cpp

---
+++ 62. Off By One.cpp
@@ -0,0 +1,11 @@
+// url = https://www.codechef.com/problems/OFFBY1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x,y;
+    cin>>x>>y;
+    cout<<(((x+y)*10)+1)<<endl;
+}
