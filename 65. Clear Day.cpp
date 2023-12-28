987addeee649eef92da395806840cb2d93944a0c
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-28 10:52:42 +0600

    Create 65. Clear Day.cpp

---
+++ 65. Clear Day.cpp
@@ -0,0 +1,11 @@
+// url = https://www.codechef.com/problems/CLEARDAY
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x,y;
+    cin>>x>>y;
+    cout<<7-(x+y)<<endl;
+}
