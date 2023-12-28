d13b3d48f7616ed61666f548a5489ee8378b2aef
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-28 10:44:13 +0600

    Create 62. 404 Not Found.cpp

---
+++ 62. 404 Not Found.cpp
@@ -0,0 +1,12 @@
+// url = https://www.codechef.com/problems/ERROR404
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n;
+    cin>>n;
+    if(n==404) cout<<"NOT FOUND"<<endl;
+    else cout<<"FOUND"<<endl;
+}
