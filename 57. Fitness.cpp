2b332c3d3de7de88281cf36e692d75ee32eb482d
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-28 10:28:36 +0600

    Create 57. Fitness.cpp

---
+++ 57. Fitness.cpp
@@ -0,0 +1,15 @@
+// url = https://www.codechef.com/problems/FIT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        cout<<10*x<<endl;
+    }
+
+}
