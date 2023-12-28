0cde2854c8d0a2953ac41dd0525e8cf2aa250260
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-28 11:05:51 +0600

    Create 69. Course Registration.cpp

---
+++ 69. Course Registration.cpp
@@ -0,0 +1,16 @@
+// url = https://www.codechef.com/problems/COURSEREG
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m,k;
+        cin>>n>>m>>k;
+        if(m>=(k+n)) cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
+    }
+}
